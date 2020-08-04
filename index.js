c69aa1b95aeb7743357376ef8b065dcc241c4139
Pekka Sinkkonen <peksin@protonmail.com>
2020-08-04 23:47:37 +0300

    osa3 teht. 3.5 ja 3.6

--- index.js
+++ index.js
@@ -28,6 +28,16 @@ let persons = [
 
 ]
 
+
+const generateId = (min, max) => {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+
+    return Math.floor(Math.random() * (max - min)) + min
+}
+
+
+// get info
 app.get('/', (req, res) => {
     res.send(`<h1>Hello world!</h1>`)
 })
@@ -71,4 +81,34 @@ app.delete('/api/persons/:id', (req, res) => {
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
+})
+
+// create one contact
+app.post('/api/persons', (req, res) => {
+
+    // bodyssa tulee asiakkaan lahettamat tiedot
+    const body = req.body
+
+    // jos tyhja nimi tai tyhja numero
+    if (!body.name || !body.number) {
+        // 400 bad request
+        return res.status(400).json({
+            error: 'name or number missing'
+        })
+    }
+
+    if (persons.filter(person => person.name === body.name).length !== 0) {
+        return res.status(409).json({
+            error: 'name already in phonebook'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(10, 1000),
+    }
+
+    persons = persons.concat(person)
+    res.json(person)
 })
