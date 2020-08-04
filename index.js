f1305506594a132b285617df117c9847dc49508c
Pekka Sinkkonen <peksin@protonmail.com>
2020-08-04 16:12:20 +0300

    osa3 teht 3.3

--- index.js
+++ index.js
@@ -47,6 +47,18 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+// get one contact
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
