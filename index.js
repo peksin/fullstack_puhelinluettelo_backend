b40064e275d7c165a4470110a94a6a691d299af3
Pekka Sinkkonen <peksin@protonmail.com>
2020-08-04 16:20:42 +0300

    osa3 teht 3.4

--- index.js
+++ index.js
@@ -59,6 +59,15 @@ app.get('/api/persons/:id', (req, res) => {
     }
 })
 
+// delete one contact
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    console.log(`${id}`)
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
