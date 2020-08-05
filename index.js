33ef527c2d0287d02dad3908418eeb20e784395f
Pekka Sinkkonen <peksin@protonmail.com>
2020-08-05 18:53:22 +0300

    osa3 teht. 3.9

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const {response} = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
+
 
 morgan.token('type', (req, res) => JSON.stringify(req.body))
 
